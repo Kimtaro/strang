2e1c9718ec45d10dfd2057c6a265207c63fc84ac
Kim Ahlström <kim.ahlstrom@gmail.com>
2011-02-16 13:56:22 +0900

    Initial commit

---
+++ strang.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'thor'
+require 'pp'
+
+class Fixnum
+  def chr_u
+    [self].pack('U*')
+  end
+end
+
+class TextAnalyzer
+  def initialize(text)
+    @text = text.to_s
+  end
+  
+  def analyze
+    # Unicode U+ZZZZ codepoints to their characters
+    @text.gsub! /U\+(\w+)/ do |m|
+      $1.to_i(16).chr_u
+    end
+    @text
+  end  
+end
+
+class CLI < Thor
+  desc 'analyze File', "Analyzes File and spits it back out on STDOUT"
+  def analyze(file)
+    f = File.open(file, 'rb')
+    text = f.read
+    puts TextAnalyzer.new(text).analyze
+  end
+end
+
+CLI.start
